2f76db5e80903371dc8956f30ca845cb76df83a6
Saravanan43 <nithishviswanatha@gmail.com>
2023-06-05 16:52:20 +0530

    Add[Anagram]

---
+++ anagram.js
@@ -0,0 +1,8 @@
+const string1 = "listen";
+const string2 = "silent";
+
+const s1 = string1.split("").sort().join("");
+const s2 = string2.split("").sort().join("");
+
+if (s1 === s2) console.log("Both are anagrams");
+else console.log("Both are not anagrams");
